85636bfe86d7755d7dbbae1f996659b1f4b84ac1
procodeeu <124219210+procodeeu@users.noreply.github.com>
2025-07-10 03:51:02 +0200

    fix: expo works for android for /mobile-app

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -4,41 +4,22 @@ export default {
     slug: "my-affirms-mobile",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
     platforms: ["ios", "android", "web"],
     splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
       backgroundColor: "#1a1a1a"
     },
-    assetBundlePatterns: [
-      "**/*"
-    ],
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.myaffirms.mobile"
     },
     android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#1a1a1a"
-      },
       package: "com.myaffirms.mobile"
     },
     web: {
-      favicon: "./assets/favicon.png",
       bundler: "metro"
     },
     plugins: [],
-    scheme: "my-affirms",
-    extra: {
-      router: {
-        origin: false
-      },
-      eas: {
-        projectId: "your-eas-project-id"
-      }
-    }
+    scheme: "my-affirms"
   }
 }
